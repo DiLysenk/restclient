58e2e02a4c2dd5836d3211d3eac7296941b392e7
ValeriyMenshikov <team095hunter@gmail.com>
2023-03-22 10:23:49 +0300

    создал пакет рест клиент

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+REQUIRES = [
+    'requests',
+    'structlog',
+    'curlify',
+    'allure-pytest'
+]
+
+setup(
+    name='restclient',
+    version='0.0.1',
+    packages=['restclient'],
+    url='https://github.com/ValeriyMenshikov/restclient.git',
+    license='MIT',
+    author='Valeriy Menshikov',
+    author_email='-',
+    install_requires=REQUIRES,
+    description='restclient with allure and logger'
+)
